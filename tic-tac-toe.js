afd840f36c9bdc2d9c24cc405f0259c53c921d86
Jada-Ann Vite <jadaannv@gmail.com>
2023-10-22 10:14:56 -0500

    Completed Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -15,37 +15,48 @@ document.addEventListener('DOMContentLoaded', (event) => {
         cell.setAttribute("class", "square");
         cell.id = c;
 
+        // Exercise 3: hover; place before click listener
+         cell.addEventListener("mouseover", function() {
+            cell.classList.add("hover");
+        })
+
+        cell.addEventListener("mouseout", function() {
+            cell.classList.remove("hover")
+        })
+
         // Exercise 2: add X or O when player clicks
         cell.addEventListener("click", function() {
             if(cell.textContent == 0 && gameOver == false){
 
                 if (isEven(play) == true){
                     cell.textContent = "O";
-                    cell.classList.add(".square.O");
+                    cell.classList.add("square", "O");
                     boardTiles[c] = "O";
                     //console.log(play);
                     infoDisplay.textContent = "It is now X's turn."
+                    //winner(infoDisplay, boardTiles);
                 }
                 else if (isEven(play)== false) {
                     cell.textContent = "X";
-                    cell.classList.add(".square.X");
-                    //cell.querySelector(".square.X");
+                    cell.classList.add("square", "X");
                     boardTiles[c] = "X";
                     infoDisplay.textContent = "It is now O's turn."
+                    //winner(infoDisplay, boardTiles);
                 }
+                
+                gameOver = winner(infoDisplay, boardTiles);
                 play++;
             }
         
-        // Exercise 3: hover
-        cell.addEventListener("mouseover", function() {
-            const cellStyle = document.querySelectorAll(".hover");
-            cellStyle.classList.add(".hover");
-        })
-        cell.addEventListener("mouseout", function() {
-            cell.querySelectorAll(".hover.O")
-        })
+       
 
-        })
+        });
+
+        //Exercise 5: reset game
+        /*let reset = document.querySelector(".btn");
+        reset.addEventListener ("click", function() {
+
+        }); */
 
     }
 
@@ -59,8 +70,42 @@ function isEven(num){
     else {
         return false;
     }
+}
+
+// Exercise 4: Check for winner
+function winner(infoDisplay, boardTiles){
+    let winningCells = [
+        [0,1,2], 
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    for (let i = 0; i < winningCells.length; i++) {
+        let row = winningCells[i];
+
+        if (
+            boardTiles[row[0]] == boardTiles[[row[1]]] && boardTiles[row[1]] == boardTiles[row[2]]
+            ) {
+            infoDisplay.textContent = ('Congratulations! ' + boardTiles[row[0]] + ' is the Winner!');
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
+
+
+
+
 
 }
+
 
  
 
