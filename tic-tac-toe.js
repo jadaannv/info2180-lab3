9919c7c82b5f8e32964d81f6a37a8a745fef72c7
Jada-Ann Vite <jadaannv@gmail.com>
2023-10-21 23:36:02 -0500

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,56 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     //initialising any varibales to be used 
+    let play = 1;  // player's turn
+    let infoDisplay = document.getElementById("status");  // status message on screen
+    let gameOver = false;
 
-    
-    //initialise the array to keep track of the current state of the game    
-    let boardTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    //initialise an array to keep track of the current state of the game    
+    let boardTiles = ["", "", "", "", "", "", "", "", ""];
 
+    // display the game board 
     let board = document.getElementById("board").children;
     for (let c = 0; c < board.length; c++){
 
         const cell = board[c];
         cell.setAttribute("class", "square");
+        cell.id = c;
+
+        //add X or O when player clicks
+        cell.addEventListener("click", function() {
+            if(cell.textContent == 0 && gameOver == false){
+
+                if (isEven(play) == true){
+                    cell.textContent = "O";
+                    cell.classList.add(".square.O");
+                    boardTiles[c] = "O";
+                    //console.log(play);
+                    infoDisplay.textContent = "It is now X's turn."
+                }
+                else if (isEven(play)== false) {
+                    cell.textContent = "X";
+                    cell.classList.add(".square.X");
+                    boardTiles[c] = "X";
+                    infoDisplay.textContent = "It is now O's turn."
+                }
+                play++;
+            }
+        })
 
     }
 
 });
 
+
+function isEven(num){
+    if (num % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+ 
 
 
